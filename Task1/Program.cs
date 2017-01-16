9a74ab9bc9a7eefde4b1602a3b88289f183757ff
zzarkymova <zzarkymova@gmail.com>
2017-01-16 17:07:09 +0600

    initial commit Lab1

---
+++ Task1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    public class Student {
+       public string first_name;
+       public string last_name;
+       public int gpa;
+       public int age;
+
+       public string GetInfo()
+       {
+
+           return first_name + " " + last_name+ " " + gpa + " " + age;
+       }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student a = new Student();
+            a.first_name="Zere";
+            a.last_name = "Zarkymova";
+            a.gpa = int.Parse("3");
+            a.age = int.Parse("17");
+            Console.WriteLine(a.GetInfo());
+
+        
+        }
+    }
+}
---
+++ Task2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] a)
+        {
+            string line = Console.ReadLine();
+            string[] numbers = line.Split(' ');
+            int[] numb=int.Parse(numbers[]);
+            int counter=0;
+            for (int i=0; i<numb.Length; ++i){
+                for (int j=2; j<numb[j]-1; ++j){
+                    if (numb[]%j==0){
+                        counter=counter+1;
+                    }
+                }
+            }
+            if (counter==0){
+                Console.WriteLine("+++++");
+            }
+            else {
+                Console.WriteLine("-----");
+            
+            }
+
+        }
+    }
+}
